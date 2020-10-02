8f47011c7de7df9b75bfa5ca51cf7527b493ce42
Cameron Garner <camerongarner9@gmail.com>
2020-10-02 12:51:10 -0400

    finished the sprint challenge

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,22 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+   let div = document.createElement("div")
+   let span1 =document.createElement("span")
+   let h1 =  document.createElement("h1")
+   let span2 =  document.createElement("span")
+   div.classList.add("header")
+   span1.classList.add("date")
+   span1.textContent = "MARCH 28, 2020"
+   h1.textContent = "Lambda Times"
+   span2.classList.add("temp")
+   span2.textContent = "98°"
+   div.appendChild(span1)
+   div.appendChild(h1)
+   div.appendChild(span2)
+   return div
+}
+
+let headcontainer = document.querySelector(".header-container")
+headcontainer.appendChild(Header())
